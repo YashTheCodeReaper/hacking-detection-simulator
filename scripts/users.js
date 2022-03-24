39b4f43a44096d0fc6e6bb1ba5d5073c9284ae1e
YashTheCodeReaper <yash.codereaper@gmail.com>
2022-03-24 12:07:10 +0530

    open close form

--- scripts/users.js
+++ scripts/users.js
@@ -1,1 +1,17 @@
 "use strict";
+
+document.addEventListener("DOMContentLoaded", function () {
+  const addUserMainButton = document.querySelector(".add-user-main-button");
+  const formOverlay = document.querySelector(".form-overlay");
+  const cancelAddUserButton = document.getElementById("cancel-button");
+
+  addUserMainButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    formOverlay.style.transform = "translateY(0%)";
+  });
+
+  cancelAddUserButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    formOverlay.style.transform = "translateY(-100%)";
+  });
+});
