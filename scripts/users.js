2ba46dafc39d0a03d3604b4af186b500f400960b
YashTheCodeReaper <yash.codereaper@gmail.com>
2022-03-24 13:11:48 +0530

    generate random users fix

--- scripts/users.js
+++ scripts/users.js
@@ -10,6 +10,9 @@ document.addEventListener("DOMContentLoaded", function () {
   const formOverlay = document.querySelector(".form-overlay");
   const cancelAddUserButton = document.getElementById("cancel-button");
   const addUserButton = document.getElementById("add-button");
+  const randomUserGenerateButton = document.querySelector(
+    ".random-user-button"
+  );
 
   // open form functionality
   addUserMainButton.addEventListener("click", function (e) {
@@ -49,5 +52,66 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // close the form
     formOverlay.style.transform = "translateY(-100%)";
+  });
+
+  // generate 30 random users
+  randomUserGenerateButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    const sampleNames = [
+      "Bruce",
+      "Tony",
+      "Peter",
+      "Banner",
+      "Natasha",
+      "Jimmy",
+      "Falcon",
+      "John",
+      "Romanoff",
+      "Lincon",
+    ];
+    const samplePhones = [
+      "8865321459",
+      "9987413658",
+      "6635412035",
+      "8852364179",
+      "1254789635",
+      "2424245785",
+      "4234534785",
+      "9634523254",
+      "8242435721",
+      "7452756782",
+    ];
+    const sampleCountry = [
+      "BRAZIL",
+      "INDIA",
+      "USA",
+      "RUSSIA",
+      "NORWAY",
+      "CHINA",
+      "CHILE",
+      "CZECH",
+      "WAKANDA",
+      "SWITZERLAND",
+    ];
+
+    for (let i = 0; i < 10; i++) {
+      const randomUser = new UserLogic(
+        sampleNames[i],
+        sampleCountry[i],
+        samplePhones[i]
+      );
+      usersArray.push(randomUser);
+
+      // append a new html tr for the generated user
+      const html = `
+          <tr>
+            <td>${randomUser.name}</td>
+            <td>${randomUser.phone}</td>
+            <td>${randomUser.country}</td>
+            <td>${randomUser.status}</td>
+          </tr>
+            `;
+      $("table").append(html);
+    }
   });
 });
