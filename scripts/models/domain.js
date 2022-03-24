6f6b606ebaa7e5e45f80b972d72ec6aa7eb53c58
YashTheCodeReaper <yash.codereaper@gmail.com>
2022-03-24 11:30:09 +0530

    create user and domain model

---
+++ scripts/models/domain.js
@@ -0,0 +1,8 @@
+// domain class
+export class Domain {
+  constructor(name, ip, owner) {
+    this.name = name;
+    this.ip = ip;
+    this.owner = owner;
+  }
+}
---
+++ scripts/models/user.js
@@ -0,0 +1,8 @@
+// user class
+export class User {
+  constructor(name, country, phone) {
+    this.name = name;
+    this.country = country;
+    this.phone = phone;
+  }
+}
