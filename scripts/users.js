5c60686bce52aa044be2e69b0830877503e131ad
YashTheCodeReaper <yash.codereaper@gmail.com>
2022-03-24 12:46:52 +0530

    add comments, finish users.js

--- scripts/users.js
+++ scripts/users.js
@@ -1,34 +1,43 @@
 "use strict";
 
+// import UserLogic and usersArray from user_logic.js
 import { UserLogic, usersArray } from "./business_logic/user_logic.js";
 
+// after DOM tree creation
 document.addEventListener("DOMContentLoaded", function () {
+  // element declarations
   const addUserMainButton = document.querySelector(".add-user-main-button");
   const formOverlay = document.querySelector(".form-overlay");
   const cancelAddUserButton = document.getElementById("cancel-button");
   const addUserButton = document.getElementById("add-button");
 
+  // open form functionality
   addUserMainButton.addEventListener("click", function (e) {
     e.preventDefault();
     formOverlay.style.transform = "translateY(0%)";
   });
 
+  // close form functionality
   cancelAddUserButton.addEventListener("click", function (e) {
     e.preventDefault();
     formOverlay.style.transform = "translateY(-100%)";
   });
 
+  // add user to usersArray by deriving the object from UserLogic class
   addUserButton.addEventListener("click", function (e) {
     e.preventDefault();
     let userName = document.getElementById("user-name").value;
     let userPhone = document.getElementById("user-phone").value;
     let userCountry = document.getElementById("user-country").value;
 
-      if (!userName || !userCountry || !userPhone) return;
-      const user = new UserLogic(userName, userCountry, userPhone);
-      usersArray.push(user);
+    // dheck for null
+    if (!userName || !userCountry || !userPhone) return;
+    // if no null, create UserLogic and add it to usersArray
+    const user = new UserLogic(userName, userCountry, userPhone);
+    usersArray.push(user);
 
-      const html = `
+    // append a new html tr for the added user
+    const html = `
           <tr>
             <td>${user.name}</td>
             <td>${user.phone}</td>
@@ -38,6 +47,7 @@ document.addEventListener("DOMContentLoaded", function () {
             `;
     $("table").append(html);
 
+    // close the form
     formOverlay.style.transform = "translateY(-100%)";
   });
 });
